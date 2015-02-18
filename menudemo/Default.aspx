c43e8b74f48671e327bd8d93eaf2a2577dba6948
unknown <mchiranj@ASI-INA94109YB.BLR.ALLEGISINDIA.COM>
2015-02-18 18:32:06 +0530

    testing

---
+++ menudemo/Default.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="menudemo._Default" %>
+
+<asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
+    <section class="featured">
+        
+    </section>
+</asp:Content>
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+   
+    
+</asp:Content>
---
+++ menudemo/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace menudemo
+{
+    public class TopMenu
+    {
+        public string Id { get; set; }
+        public string TopName { get; set; }
+        public string Url { get; set; }
+    }
+    public class LeftMenu
+    {
+        public string ParentId { get; set; }
+        public string LeftMenuName { get; set; }
+        public string LeftMenuUrl { get; set; }
+        public string OrderbyID { get; set; }
+        public int RoldId {get;set; }
+    }
+   
+}
